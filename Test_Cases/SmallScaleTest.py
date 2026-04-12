7e33faead9b81b0fea57e4b332ea985344b36aa6
Cookies745 <joshualimyonjian@gmail.com>
2026-04-12 21:22:04 +0100

    developed for Small and full scale test

---
+++ Test_Cases/SmallScaleTest.py
@@ -0,0 +1,34 @@
+from OneRotation import oneRotation
+import random
+import time
+
+strategyList = ["Random", "Greed", "Corner", "Frontier"]
+
+drawScore, blackStrategyScore, whiteStrategyScore = 0, 0, 0
+random.seed("JoshuaLimYon")
+
+if __name__ == "__main__":
+    startTime = time.time()
+
+    for blackStrategy in strategyList:
+        for whiteStrategy in strategyList:
+            blackStrategyScore, whiteStrategyScore, drawScore = oneRotation(blackStrategy, whiteStrategy, 50)
+
+
+            print("Black Strategy:", blackStrategy)
+            print("Black Win Score:", blackStrategyScore)
+
+            print("-" * 15)
+            print("White Strategy:", whiteStrategy)
+            print("White Win Score:", whiteStrategyScore)
+
+            print("-" * 15)
+            print("Draw Score:", drawScore)
+            print("-" * 15)
+
+            print()
+            print()
+
+    endTime = time.time()
+
+    print("Time Taken:", (endTime - startTime))
